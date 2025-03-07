aa45ba110eab4d7619fed193b40bdf95ab5fd235
Ryan Hartung <rhartung@crcfe01.crc.nd.edu>
2025-03-07 14:30:50 -0500

    updated shell file

--- scripts/ryans_scripts/foamrun.sh
+++ scripts/ryans_scripts/foamrun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# rm -rf "./temp/squarecil"
+rm -rf "./temp/squarecil"
 
 # activate conda environment
 conda init
@@ -15,7 +15,15 @@ mkdir "temp"
 cd "./temp"
 
 wget http://www.wolfdynamics.com/wiki/squarecil.tar.gz
-# sh run_solver.sh
+tar -xvf squarecil.tar.gz
+
+cd "./squarecil/RANS"
+
+# add lines to solver script
+echo "mpirun -np 4 simpleFoam -postProcess -func Q -parallel" >> run_solver.sh
+echo "mpirun -np 4 simpleFoam -postProcess -func yPlus -parallel" >> run_solver.sh
+
+echo "reconstructPar" >> run_solver.sh
 echo "paraFoam -builtin" >> run_solver.sh
 
 # run solver
