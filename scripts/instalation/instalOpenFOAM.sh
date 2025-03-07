8325dea3e1dc0577e0286fa7bac623729680d135
Liubov Kurafeeva <liubov_kurafeeva@ucsb.edu>
2025-03-07 09:28:19 -0800

    Update instalOpenFOAM.sh

--- scripts/instalation/instalOpenFOAM.sh
+++ scripts/instalation/instalOpenFOAM.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
+# Notre Dame cluster
+module load openfoam/10.0
+
+# For separate machines
 sudo sh -c "wget -O - https://dl.openfoam.org/gpg.key > /etc/apt/trusted.gpg.d/openfoam.asc"
 sudo add-apt-repository http://dl.openfoam.org/ubuntu
-sudo add-apt-repository "http://dl.openfoam.org/ubuntu dev"
 sudo apt-get update
-sudo apt-get -y install openfoam-dev
-
-
-source /opt/openfoam-dev/etc/bashrc
-foamVersion
+sudo apt-get -y install openfoam10
