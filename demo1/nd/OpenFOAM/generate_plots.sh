ae28e6c9ded6d77bd85bc27516a4dda19a6585f7
Ryan Hartung <rhartung@nd.edu>
2025-05-13 17:00:18 -0400

    service now connect OpenFOAM

---
+++ demo1/nd/OpenFOAM/generate_plots.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source ~/.bashrc
+conda activate nd-xgfabric
+
+# add OpenFOAM module to computer
+module add openfoam/10.0/gcc/8.5.0
+module add paraview/5.11.2
+
+cd damBreak
+
+foamToVTK -allPatches
+
+cd ..
+
+pvpython --force-offscreen-rendering render_foam.py
+
+python create_gif.py
--- demo1/nd/service/submit.sh
+++ demo1/nd/service/submit.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-qsub launch.sh
+# Check for the DISPLAY environment variable
+if [ -z "$DISPLAY" ]; then # there is no DISPLAY env
+    echo "launching with no display env"
+    qsub launch.sh
+    cd ../OpenFOAM
+    sh generate_plots.sh
+else # there is a DISPLAY env
+    echo "launching with a display env"
+    sh launch.sh
+fi
