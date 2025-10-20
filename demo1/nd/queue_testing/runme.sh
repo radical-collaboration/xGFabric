59172f9e09437604d8c7abecfc0080bb9320e2b9
Ryan Hartung <rhartung@nd.edu>
2025-10-20 15:57:58 -0400

    added support for ANVIL

--- demo1/nd/queue_testing/runme.sh
+++ demo1/nd/queue_testing/runme.sh
@@ -14,81 +14,60 @@ fi
 
 idx="$(tail -n 1 data/index.log)"
 p_start=$(date '+%s')
+machine=1 # ANVIL
+# machine=2 # Notre Dame
+# machine=3 # TACC Stampede3
+
+declare -a cores=("4" "16" "64")
+
 while [ true ]; do
+    for num_cores in "${cores[@]}"; do
+        cp template.sh "scripts/submit_${num_cores}_${idx}.sh"
 
-# -------------------------------------------------
-#                 4 core jobs
-# -------------------------------------------------
-    cp template.sh "scripts/submit_4_$idx.sh"
+        if [ $machine -eq 1 ]; then # ANVIL
+            line_to_prepend="#!/bin/bash\nsubmitted_at=$(date '+%s.%N')\nfile=submit_${num_cores}_${idx}.sh\nnum_cores=${num_cores}\n"
+            file="scripts/submit_${num_cores}_${idx}.sh"
+            sed -i "1i $line_to_prepend" "$file"
 
-    line_to_prepend="#!/bin/bash\n#$ -q long\n#$ -pe smp 4\nsubmitted_at=$(date '+%s.%N')\nfile=submit_4_$idx.sh\nnum_cores=4\n"
-    file="scripts/submit_4_$idx.sh"
-    sed -i "1i $line_to_prepend" "$file"
+            job_id=$(sbatch --ntasks="$num_cores" --mem="16GB" --time="1:0:0" --parsable --output="logs/job_output_%j.out" "$file")
 
-    qsub_output=$(qsub -o "logs/job_output_\$JOB_ID.out" "$file")
-    job_id=$(echo "$qsub_output" | grep -oE '[0-9]+' | head -1)
+            jobs_ahead=$(squeue -p long -t PD -o "%.18i" | tail -n +2 | grep -n "^$job_id$" | cut -d: -f1 | awk '{print $1-1}')
+            jobs_ahead=${jobs_ahead:-0}
+            jobs_in_my_queue=$(squeue -u $USER | tail -n +2 | wc -l)
+            jobs_running=$(squeue -u $USER -t R | tail -n +2 | wc -l)
 
-    jobs_ahead=$(qstat -q long -s p | awk -v job_id="$job_id" 'NR>1 {if($1==job_id) exit; count++} END {print count}')
-    jobs_in_my_queue=$(qstat -u $USER | tail -n +3 | wc -l)
-    jobs_running=$(qstat -u $USER -s r | tail -n +3 | wc -l)
-    echo "Submitted job ==> $job_id"
-    echo "ID =============> $idx"
-    echo "Jobs ahead =====> $jobs_ahead"
-    echo "Running for ====> $(bc -l <<< "$(date '+%s') - $p_start") seconds"
-    echo "------------------------------------"
-    echo "submit_4_$idx.sh,$jobs_ahead,$jobs_in_my_queue,$jobs_running,4" >> data/ahead.csv
+            echo "Submitted job ==> $job_id"
+            echo "ID =============> $idx"
+            echo "Jobs ahead =====> $jobs_ahead"
+            echo "Running for ====> $(bc -l <<< "$(date '+%s') - $p_start") seconds"
+            echo "------------------------------------"
+            echo "submit_${num_cores}_${idx}.sh,$jobs_ahead,$jobs_in_my_queue,$jobs_running,${num_cores}" >> data/ahead.csv
 
+        elif [ $machine -eq 2 ]; then # Notre Dame
+            line_to_prepend="#!/bin/bash\n#$ -q long\n#$ -pe smp $num_cores\nsubmitted_at=$(date '+%s.%N')\nfile=submit_${num_cores}_${idx}.sh\nnum_cores=${num_cores}\n"
+            file="scripts/submit_${num_cores}_${idx}.sh"
+            sed -i "1i $line_to_prepend" "$file"
 
+            qsub_output=$(qsub -o "logs/job_output_\$JOB_ID.out" "$file")
+            job_id=$(echo "$qsub_output" | grep -oE '[0-9]+' | head -1)
 
-# -------------------------------------------------
-#                16 core jobs
-# -------------------------------------------------
+            jobs_ahead=$(qstat -q long -s p | awk -v job_id="$job_id" 'NR>1 {if($1==job_id) exit; count++} END {print count}')
+            jobs_in_my_queue=$(qstat -u $USER | tail -n +3 | wc -l)
+            jobs_running=$(qstat -u $USER -s r | tail -n +3 | wc -l)
 
-    cp template.sh "scripts/submit_16_$idx.sh"
+            echo "Submitted job ==> $job_id"
+            echo "ID =============> $idx"
+            echo "Jobs ahead =====> $jobs_ahead"
+            echo "Running for ====> $(bc -l <<< "$(date '+%s') - $p_start") seconds"
+            echo "------------------------------------"
+            echo "submit_${num_cores}_${idx}.sh,$jobs_ahead,$jobs_in_my_queue,$jobs_running,${num_cores}" >> data/ahead.csv
 
-    line_to_prepend="#!/bin/bash\n#$ -q long\n#$ -pe smp 16\nsubmitted_at=$(date '+%s.%N')\nfile=submit_16_$idx.sh\nnum_cores=16\n"
-    file="scripts/submit_16_$idx.sh"
-    sed -i "1i $line_to_prepend" "$file"
-
-    qsub_output=$(qsub -o "logs/job_output_\$JOB_ID.out" "$file")
-    job_id=$(echo "$qsub_output" | grep -oE '[0-9]+' | head -1)
-
-    jobs_ahead=$(qstat -q long -s p | awk -v job_id="$job_id" 'NR>1 {if($1==job_id) exit; count++} END {print count}')
-    jobs_in_my_queue=$(qstat -u $USER | tail -n +3 | wc -l)
-    jobs_running=$(qstat -u $USER -s r | tail -n +3 | wc -l)
-    echo "Submitted job ==> $job_id"
-    echo "ID =============> $idx"
-    echo "Jobs ahead =====> $jobs_ahead"
-    echo "Running for ====> $(bc -l <<< "$(date '+%s') - $p_start") seconds"
-    echo "------------------------------------"
-    echo "submit_16_$idx.sh,$jobs_ahead,$jobs_in_my_queue,$jobs_running,16" >> data/ahead.csv
-
-
-# -------------------------------------------------
-#                64 core jobs
-# -------------------------------------------------
-
-    cp template.sh "scripts/submit_64_$idx.sh"
-
-    line_to_prepend="#!/bin/bash\n#$ -q long\n#$ -pe smp 64\nsubmitted_at=$(date '+%s.%N')\nfile=submit_64_$idx.sh\nnum_cores=64\n"
-    file="scripts/submit_64_$idx.sh"
-    sed -i "1i $line_to_prepend" "$file"
-
-    qsub_output=$(qsub -o "logs/job_output_\$JOB_ID.out" "$file")
-    job_id=$(echo "$qsub_output" | grep -oE '[0-9]+' | head -1)
-
-    jobs_ahead=$(qstat -q long -s p | awk -v job_id="$job_id" 'NR>1 {if($1==job_id) exit; count++} END {print count}')
-    jobs_in_my_queue=$(qstat -u $USER | tail -n +3 | wc -l)
-    jobs_running=$(qstat -u $USER -s r | tail -n +3 | wc -l)
-    echo "Submitted job ==> $job_id"
-    echo "ID =============> $idx"
-    echo "Jobs ahead =====> $jobs_ahead"
-    echo "Running for ====> $(bc -l <<< "$(date '+%s') - $p_start") seconds"
-    echo "------------------------------------"
-    echo "submit_64_$idx.sh,$jobs_ahead,$jobs_in_my_queue,$jobs_running,64" >> data/ahead.csv
-
+        elif [ $machine -eq 3 ]; then # TACC Stampede3
+            job_id=$(sbatch --time="1:0:0" --nodes="1" --partition="skx" --ntasks="$num_cores" --mem="16GB" --parsable --output="logs/job_output_%j.out" "$file" | tail -n 1)
+        fi
+    done
 
     idx=$((idx + 1))
-    echo "$idx" >> index.log
+    echo "$idx" >> data/index.log
     sleep 5m
 done
