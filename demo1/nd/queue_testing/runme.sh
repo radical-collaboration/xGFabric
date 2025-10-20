5bdeccd1456a19c3e84aa494a763222f1435e583
Ryan Hartung <rhartung@nd.edu>
2025-10-20 16:29:21 -0400

    Add job submission logic for TACC Stampede3

--- demo1/nd/queue_testing/runme.sh
+++ demo1/nd/queue_testing/runme.sh
@@ -63,7 +63,24 @@ while [ true ]; do
             echo "submit_${num_cores}_${idx}.sh,$jobs_ahead,$jobs_in_my_queue,$jobs_running,${num_cores}" >> data/ahead.csv
 
         elif [ $machine -eq 3 ]; then # TACC Stampede3
-            job_id=$(sbatch --time="1:0:0" --nodes="1" --partition="skx" --ntasks="$num_cores" --mem="16GB" --parsable --output="logs/job_output_%j.out" "$file" | tail -n 1)
+
+            line_to_prepend="#!/bin/bash\nsubmitted_at=$(date '+%s.%N')\nfile=submit_${num_cores}_${idx}.sh\nnum_cores=${num_cores}\n"
+            file="scripts/submit_${num_cores}_${idx}.sh"
+            sed -i "1i $line_to_prepend" "$file"
+
+            job_id=$(sbatch --time="1:0:0" --nodes="1" --partition="icx" --ntasks="$num_cores" --mem="16GB" --parsable --output="logs/job_output_%j.out" "$file" | tail -n 1)
+
+            jobs_ahead=$(squeue -p long -t PD -o "%.18i" | tail -n +2 | grep -n "^$job_id$" | cut -d: -f1 | awk '{print $1-1}')
+            jobs_ahead=${jobs_ahead:-0}
+            jobs_in_my_queue=$(squeue -u $USER | tail -n +2 | wc -l)
+            jobs_running=$(squeue -u $USER -t R | tail -n +2 | wc -l)
+
+            echo "Submitted job ==> $job_id"
+            echo "ID =============> $idx"
+            echo "Jobs ahead =====> $jobs_ahead"
+            echo "Running for ====> $(bc -l <<< "$(date '+%s') - $p_start") seconds"
+            echo "------------------------------------"
+            echo "submit_${num_cores}_${idx}.sh,$jobs_ahead,$jobs_in_my_queue,$jobs_running,${num_cores}" >> data/ahead.csv
         fi
     done
 
