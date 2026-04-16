cd686e60dec5de8cd5b469d32d696d74f8333282
Ryan Hartung <rhartung@nd.edu>
2026-04-16 13:35:11 -0400

    Update to NERSC env

--- demo2/nd/CFDaAI/simulation/run_simulation.sh
+++ demo2/nd/CFDaAI/simulation/run_simulation.sh
@@ -33,7 +33,16 @@ if [ "$SYSTEM_TYPE" == "nd" ]; then
     module add openfoam/10.0/gcc/11.5.0 > /dev/null 2>&1
     module add paraview/5.11.2
 elif [ "$SYSTEM_TYPE" == "nersc" ]; then
-    source "${WORK_DIR}/env/env_nersc.sh"
+    module --force purge
+    module load spack
+    . /global/common/software/nersc9/spack/1.1.0/share/spack/setup-env.sh
+    module load conda
+    module load paraview || true  # paraview may not be needed at runtime
+    spack load openmpi@4.1.5
+    group_num=$(groups | awk -F" " '{print $2}')
+    export OPENFOAM_ROOT="/global/common/software/$group_num/openfoam"
+    source "$OPENFOAM_ROOT/OpenFOAM-dev/etc/bashrc" || true  # non-fatal: OF env may already be set
+    export LD_LIBRARY_PATH=$(spack location -i openmpi@4.1.5)/lib:$LD_LIBRARY_PATH
 elif [ -n "$FOAM_BASH" ]; then
     source $FOAM_BASH
 elif [ -n "$FOAM_ETC" ]; then
